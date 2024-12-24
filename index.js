a612161a05f5fb7a4c375f884039f21c4c8db16c
Md Shahien Khan <shawonkn58@gmail.com>
2024-12-24 13:32:01 +0000

    get functionality added for single data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // step 6 adding dotenv
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -44,7 +44,17 @@ async function run() {
         const result = await cursor.toArray();
         // const result = await serviceCollection.find({name : "test service", price : 100});
         res.send(result);
-    })
+    });
+
+    // step 9 get specific service trough id
+    app.get('/services/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await serviceCollection.findOne(query);
+        res.send(result);
+    });
+
+
 
   } finally {
     // Ensures that the client will close when you finish/error
